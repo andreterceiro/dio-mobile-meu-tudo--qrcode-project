6b78bdcb231cd31a6f01df5485bee37e6d41c8b2
André de Paula Terceiro <andre@terceiro.com.br>
2025-12-22 11:35:51 -0300

    feat: creating a sub prompt to the QRCode option

--- src/prompts/prompt-qrcode.js
+++ src/prompts/prompt-qrcode.js
@@ -1,7 +1,16 @@
 import chalk from 'chalk';
 
 const promptQRCode = [
-
+    {
+        name: "link",
+        description: chalk.yellow("Digite o link para gerar o QRCode")
+    },
+    {
+        name: "type",
+        description: chalk.yellow("Escolha o tipo entre 1-normal e 2-terminal"),
+        pattern: /^[1,2]{1}$/,
+        message: chalk.red.italic("Escolha apenas entre 1 e 2")
+    }
 ];
 
 export default promptQRCode;
