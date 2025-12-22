5a2e8b1b933121ce0c0eb2e84c4f3a0075c6761f
André de Paula Terceiro <andre@terceiro.com.br>
2025-12-22 16:10:46 -0300

    feat: using the file who generates the random passwords

--- src/services/password/create.js
+++ src/services/password/create.js
@@ -1,7 +1,10 @@
 import chalk from "chalk";
+import handler from "./handler.js"
 
 async function createPassword() {
     console.log(chalk.green("passowrd"));
+    const password = await handler();
+    console.log(password);
 }
 
 export default createPassword;
